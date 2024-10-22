1e223e9658d0bae0c2ad5f40b445709ab47fda33
Hoàng Long <hoanglong.jpg@gmail.com>
2024-10-22 19:33:14 +0700

    Hoan thien chuc nang load mon an (#14)

--- app/src/main/java/com/fastdine/utt/Food.java
+++ app/src/main/java/com/fastdine/utt/Food.java
@@ -1,9 +1,32 @@
 package com.fastdine.utt;
 
 public class Food {
-    private String food_id;
+    private String name;
     private String description;
     private String image;
-    private String name;
-    private Double price;
+    private double price;
+
+    public Food(String name, String description, String image, double price) {
+        this.name = name;
+        this.description = description;
+        this.image = image;
+        this.price = price;
+    }
+
+    // Các phương thức getter
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public double getPrice() {
+        return price;
+    }
 }
